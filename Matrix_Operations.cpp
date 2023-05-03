6f597a8749cc03e2f9eea13f01b560d019987bf7
mikibak <mikibak123@gmail.com>
2023-05-04 00:53:30 +0200

    Add Gauss-Seidel

--- Matrix_Operations.cpp
+++ Matrix_Operations.cpp
@@ -11,49 +11,55 @@
 Matrix Jacobi(const Matrix& A, const Matrix& b, const Matrix& L, const Matrix& U, const Matrix& D, int max_iter, double tol) {
     Matrix r = Matrix(1, MY_SIZE, 1);
     Matrix minus_D = Matrix(D.GetSizeX(), D.GetSizeY());
+    Matrix LU = L + U;
+    Matrix DforwardSubstitutionb =  D.forwardSubstitution(b);
+
     for(int i = 0; i < MY_SIZE; i++) {
         minus_D.SetElement(i, i, -1 *D.GetElement(i, i));
     }
-    minus_D.Print();
-    D.Print();
     // Jacobi iteration
     for (int k = 0; k < max_iter; k++) {
-        r.Print();
-        Matrix lu = L + U;
-        Matrix lur = lu * r;
-        lur.Print();
-        Matrix sub1 = minus_D.forwardSubstitution(lur);
-        std::cout << "pierwsze: ";
-        sub1.Print();
-        Matrix sub2 = D.forwardSubstitution(b);
-        std::cout << "drugie: ";
-        sub2.Print();
-        r = minus_D.forwardSubstitution((L + U) * r) + D.forwardSubstitution(b);
 
-        std::cout << "r: ";
-        r.Print();
+        r = minus_D.forwardSubstitution(LU * r) + DforwardSubstitutionb;
 
         double error = (A*r - b).Norm();
 
-
-
         if(error < tol) {
             std::cout << "Number of iterations for Jacobi: " << k << std::endl;
-            b.Print();
-            A.Print();
-            r.Print();
+            return r;
+        }
+    }
+
+    // Return the last approximation
+    return r;
+}
+
+
+Matrix GaussSeidel(const Matrix& A, const Matrix& b, const Matrix& L, const Matrix& U, const Matrix& D, int max_iter, double tol) {
+    Matrix r = Matrix(1, MY_SIZE, 1);
+    Matrix minus_DL = Matrix(D.GetSizeX(), D.GetSizeY());
+    Matrix DLforwardSubstitutionb =  (D + L).forwardSubstitution(b);
+
+    //create DL
+    for(int i = 0; i < MY_SIZE; i++) {
+        for(int j = 0; j < MY_SIZE; j++) {
+            double value = -1 * (D.GetElement(i, j) + L.GetElement(i, j));
+            minus_DL.SetElement(i, j, value);
+        }
+    }
+    // Jacobi iteration
+    for (int k = 0; k < max_iter; k++) {
+
+        //r = -(D+L) \ (U*r) + (D+L) \ b;
+        r = minus_DL.forwardSubstitution(U * r) + DLforwardSubstitutionb;
+
+        double error = (A*r - b).Norm();
+
+        if(error < tol) {
+            std::cout << "Number of iterations for Gauss-Seidel: " << k << std::endl;
             return r;
         }
 
-        // Check if the difference is smaller than the tolerance
-        /*Matrix m = r_next - r;
-        if (m.Norm() < tol) {
-            r_next.Print();
-            return r;
-        }
-
-        // Update x
-        r = r_next;*/
     }
 
     // Return the last approximation
@@ -63,30 +69,6 @@ Matrix Jacobi(const Matrix& A, const Matrix& b, const Matrix& L, const Matrix& U
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    //auto* matrix = new Matrix(MY_SIZE, MY_SIZE);
-    //Matrix* matrix_ptr = new Matrix(10, 10);
-    //Matrix matrix = *matrix_ptr;
-    //matrix.Print();
-    //188968
-    //Matrix* band_matrix = new BandMatrix(10, 10, 5 + 9, -1, -1);
-/*
-    Matrix* band_matrix = new BandMatrix(10, 10, 1, 2, 3);
-    Matrix* band_matrix2 = new BandMatrix(10, 10, 1, 2, 3);
-    Matrix matrix = *band_matrix;
-    Matrix matrix2 = *band_matrix2;
-    matrix.Print();
-    matrix += matrix2;
-    matrix.Print();
-
-    Matrix m1 = Matrix(10, 10);
-    Matrix m2 = Matrix(10, 11);
-    m2.Print();
-    m1 += m2;
-    m1.Print();
-*/
-
-
     //TASK A
 
     int a1 = 5 + 9;
@@ -103,14 +85,15 @@ int main()
         b->SetElement(0, i, value);
     }
 
-    //b->Print();
-
     Matrix* L = A->LowerTriangle();
     Matrix* U = A->UpperTriangle();
     Matrix* D = A->Diagonal();
 
-    Matrix x = Jacobi(*A, *b, *L, *U, *D, 1000, 1e-6);
+    Matrix x = Jacobi(*A, *b, *L, *U, *D, max_iter, 1e-6);
     x.Print();
+
+    Matrix x2 = GaussSeidel(*A, *b, *L, *U, *D, max_iter, 1e-6);
+    x2.Print();
     //while(true) {
 
         //r Matrix x_next = -D\(L + U) * r + D\b;
