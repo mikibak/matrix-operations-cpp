42fb4f60abde85ca6472f9890b6f18961d8c9f2e
mikibak <mikibak123@gmail.com>
2023-05-04 10:17:27 +0200

    Write time to csv

--- Matrix_Operations.cpp
+++ Matrix_Operations.cpp
@@ -5,10 +5,78 @@
 #include "Matrix.h"
 #include "BandMatrix.h"
 #include <chrono>
+#include <fstream>
 
 #define MY_SIZE 968
 
+Matrix LuDecomposition(const Matrix& A, const Matrix& b) {
+    Matrix U = A.UpperTriangle() + A.Diagonal();
+    Matrix L = A.LowerTriangle() + A.Diagonal();
 
+    int N = A.GetSizeX();
+
+    //auto start = chrono::high_resolution_clock::now();
+
+    /* creating matrices L and U, such that A = L * U */
+    for (int i = 0; i < N - 1; ++i)		//columns
+    {
+        for (int j = i + 1; j < N; ++j)	//rows
+        {
+            //L->A[j][i] = U->A[j][i] / U->A[i][i];
+            double value = U.GetElement(j, i) / U.GetElement(i, i);
+            L.SetElement(j, i, value);
+
+            for (int k = i; k < N; ++k) {
+                //U->A[j][k] = U->A[j][k] - L->A[j][i] * U->A[i][k];
+                double v = U.GetElement(j, k) - L.GetElement(j, i) * U.GetElement(i, k);
+                U.SetElement(j, k, v);
+            }
+        }
+    }
+    /* now the main equation to solve may be defined as L * U * x = b  */
+
+    /* solving equation L * y = b for y using forward substitution method */
+    Matrix x = Matrix(1, A.GetSizeY(), 0);
+    Matrix y = Matrix(1, A.GetSizeY(), 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        double S = 0;
+
+        for (int j = 0; j < i; ++j) {
+            S += L.GetElement(i, j) * y.GetElement(0, j);
+        }
+            //S += L->A[i][j] * y[j];
+
+        //y[i] = (b[i] - S) / L->A[i][i];
+        double value = (b.GetElement(0, 1) - S) / L.GetElement(i, i);
+        y.SetElement(0, i, value);
+    }
+
+    /* solving equation U * x = y using backward substitution method */
+    for (int i = N - 1; i >= 0; --i)
+    {
+        double S = 0;
+
+        for (int j = i + 1; j < N; ++j) {
+            S += U.GetElement(i, j);
+        } //S += U->A[i][j] * x[j];
+
+        double value = (y.GetElement(0, i) - S) / U.GetElement(i, i);
+        x.SetElement(0, i, value);
+        //x[i] = (y[i] - S) / U->A[i][i];
+    }
+
+    return x;
+
+    //auto end = chrono::high_resolution_clock::now();
+    //auto difference = end - start;
+    //*duration = chrono::duration<double, milli>(difference).count();
+
+    //double* r = new double[N];		//residual vector
+    //residual(A, b, x, r);
+    //*n = norm(r);
+}
 
 Matrix Jacobi(const Matrix& A, const Matrix& b, int max_iter, double tol) {
 
@@ -83,7 +151,7 @@ long TimeWrapJacobi(const Matrix& A, const Matrix& b, int max_iter, double tol) 
     Jacobi(A, b, max_iter, tol);
 
     auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     return duration.count();
 }
 
@@ -92,8 +160,55 @@ long TimeWrapGaussSeidel(const Matrix& A, const Matrix& b, int max_iter, double 
     GaussSeidel(A, b, max_iter, tol);
 
     auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     return duration.count();
+}
+
+void ValuesForPlots(Matrix* A, Matrix* b, int max_iter, double tol, int a1, int a2, int a3) {
+    //N = {100, 500, 1000, 2000, 3000 . . . }
+
+    int N[] = {10, 50, 100, 200, 300};
+    //int N[] = {100, 500, 1000, 2000, 3000};
+
+    long GaussSeidelIter[5];
+    long JacobiIter[5];
+
+    std::cout << "\n\n";
+
+    for(int i = 0; i < 5; i++) {
+        A = new BandMatrix(N[i], N[i], a1, a2, a3);
+        b = new Matrix(1, N[i]);
+        for(double j = 0; j < N[i]; j++) {
+            double value = sin( j * (8 + 1));
+            b->SetElement(0, j, value);
+        }
+        GaussSeidelIter[i] = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
+        std::cout << i << ": " << GaussSeidelIter[i] << " s" << std::endl;
+    }
+
+    std::cout << "\n";
+
+    for(int i = 0; i < 5; i++) {
+        A = new BandMatrix(N[i], N[i], a1, a2, a3);
+        b = new Matrix(1, N[i]);
+        for(double j = 0; j < N[i]; j++) {
+            double value = sin( j * (8 + 1));
+            b->SetElement(0, j, value);
+        }
+        JacobiIter[i] = TimeWrapJacobi(*A, *b, max_iter, 1e-6);
+        std::cout << i << ": " << JacobiIter[i] << " s" << std::endl;
+    }
+
+    //export to csv
+    std::ofstream out("/home/mikibak/matrix-operations-cpp/GaussSeidel.csv");
+    for (auto col : GaussSeidelIter)
+        out << col <<',';
+    out << '\n';
+
+    std::ofstream out2("/home/mikibak/matrix-operations-cpp/Jacobi.csv");
+    for (auto col : JacobiIter)
+        out2 << col <<',';
+    out2 << '\n';
 }
 
 int main()
@@ -139,82 +254,14 @@ int main()
 
 
     //C:
-    a1 = 3;
+    /*a1 = 3;
     a2 = -1;
     a3 = -1;
 
     A = new BandMatrix(MY_SIZE, MY_SIZE, a1, a2, a3);
     Jacobi(*A, *b, max_iter, 1e-6);
-    GaussSeidel(*A, *b, max_iter, 1e-6);
+    GaussSeidel(*A, *b, max_iter, 1e-6);*/
 
-    //N = {100, 500, 1000, 2000, 3000 . . . }
-    std::cout << "\n\n";
-    A = new BandMatrix(100, 100, a1, a2, a3);
-    b = new Matrix(1, 100);
-    for(double i = 0; i < 100; i++) {
-        double value = sin( i * (8 + 1));
-        b->SetElement(0, i, value);
-    }
-    long GaussSeidel100 = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
-    std::cout << "100: " << GaussSeidel100 << " s" << std::endl;
+    ValuesForPlots(A, b, max_iter, 1e-6, a1, a2, a3);
 
-    A = new BandMatrix(500, 500, a1, a2, a3);
-    b = new Matrix(1, 500);
-    for(double i = 0; i < 500; i++) {
-        double value = sin( i * (8 + 1));
-        b->SetElement(0, i, value);
-    }
-    long GaussSeidel500 = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
-    std::cout << "500: " << GaussSeidel500 << " s" << std::endl;
-
-    A = new BandMatrix(1000, 1000, a1, a2, a3);
-    b = new Matrix(1, 1000);
-    for(double i = 0; i < 1000; i++) {
-        double value = sin( i * (8 + 1));
-        b->SetElement(0, i, value);
-    }
-    long GaussSeidel1000 = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
-    std::cout << "1000: " << GaussSeidel1000 << " s" << std::endl;
-
-    for(int i = 1000; i < 4000; i+= 1000) {
-        A = new BandMatrix(i, i, a1, a2, a3);
-        b = new Matrix(1, i);
-        for(double j = 0; j < j; j++) {
-            double value = sin( j * (8 + 1));
-            b->SetElement(0, j, value);
-        }
-        long GaussSeidel500 = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
-        std::cout << i << ": " << GaussSeidel500 << " s" << std::endl;
-    }
-
-
-    std::cout << "\n\n";
-    A = new BandMatrix(100, 100, a1, a2, a3);
-    b = new Matrix(1, 100);
-    for(double i = 0; i < 100; i++) {
-        double value = sin( i * (8 + 1));
-        b->SetElement(0, i, value);
-    }
-    long Jacobi100 = TimeWrapJacobi(*A, *b, max_iter, 1e-6);
-    std::cout << "100: " << Jacobi100 << " s" << std::endl;
-
-    A = new BandMatrix(500, 500, a1, a2, a3);
-    b = new Matrix(1, 500);
-    for(double i = 0; i < 500; i++) {
-        double value = sin( i * (8 + 1));
-        b->SetElement(0, i, value);
-    }
-    long Jacobi500 = TimeWrapJacobi(*A, *b, max_iter, 1e-6);
-    std::cout << "500: " << Jacobi500 << " s" << std::endl;
-
-    for(int i = 1000; i < 4000; i+= 1000) {
-        A = new BandMatrix(i, i, a1, a2, a3);
-        b = new Matrix(1, i);
-        for(double i = 0; i < i; i++) {
-            double value = sin( i * (8 + 1));
-            b->SetElement(0, i, value);
-        }
-        long Jacobi500 = TimeWrapJacobi(*A, *b, max_iter, 1e-6);
-        std::cout << i << ": " << Jacobi500 << " s" << std::endl;
-    }
 }
