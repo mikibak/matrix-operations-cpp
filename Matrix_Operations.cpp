5552cf93fef74b3c7d5379522130caa220714b4f
mikibak <mikibak123@gmail.com>
2023-05-04 14:17:34 +0200

    Code cleanup

--- Matrix_Operations.cpp
+++ Matrix_Operations.cpp
@@ -22,32 +22,6 @@ Matrix LuDecomposition(const Matrix& A, const Matrix& b) {
     for (int i = 0; i < N; i++)
         L.SetElement(i, i, 1.0);
 
-    /*for (int j = 0; j < N; j++) {
-        for (int i = 0; i <= j; i++) {
-            double value = A.GetElement(j, i);
-            U.SetElement(j, i, U.GetElement(j, i) + value);
-            //U[i][j] += A[i][j];
-            for (int k = 0; k <= i - 1; k++) {
-                double value1 = L.GetElement(k, i) * U.GetElement(j, k);
-                U.SetElement(j, i, U.GetElement(j, i) - value1);
-            }
-            //U[i][j] -= L[i][k] * U[k][j];
-
-        }
-
-        for (int i = j+1; i < N; i++) {
-            double Lij = L.GetElement(j, i);
-            for (int k = 0; k <= j - 1; k++) {
-                L.SetElement(j, i, Lij - L.GetElement(k, i) * U.GetElement(j, k));
-                //L[i][j] -= L[i][k] * U[k][j];
-            }
-            L.SetElement(j, i, Lij + A.GetElement(j, i));
-            L.SetElement(j, i, Lij / U.GetElement(j, j));
-            //L[i][j] += A[i][j];
-            //L[i][j] /= U[j][j];
-        }
-    }*/
-
     //Doolittle
     for (int i = 0; i < N; i++)
     {
@@ -85,7 +59,6 @@ Matrix LuDecomposition(const Matrix& A, const Matrix& b) {
     //U.Print();
     //(L*U).Print();
 
-    /* solving equation L * y = b for y using forward substitution method */
     Matrix x = Matrix(1, A.GetSizeY(), 0);
     Matrix y = Matrix(1, A.GetSizeY(), 0);
 
@@ -96,61 +69,27 @@ Matrix LuDecomposition(const Matrix& A, const Matrix& b) {
         for (int j = 0; j < i; j++) {
             S += L.GetElement(j, i) * y.GetElement(0, j);
         }
-        //S += L->A[i][j] * y[j];
 
-        //y[i] = (b[i] - S) / L->A[i][i];
         double value = (b.GetElement(0, i) - S) / L.GetElement(i, i);
         y.SetElement(0, i, value);
     }
 
-    /* solving equation U * x = y using backward substitution method */
     for (int i = N - 1; i >= 0; i--)
     {
         double S = 0;
 
         for (int j = i + 1; j < N; j++) {
             S += U.GetElement(j, i) * x.GetElement(0, j);
-        } //S += U->A[i][j] * x[j];
+        }
 
         double value = (y.GetElement(0, i) - S) / U.GetElement(i, i);
         x.SetElement(0, i, value);
-        //x[i] = (y[i] - S) / U->A[i][i];
     }
-
-    /*  //Podstawienie w przód dla Ly = b
-      for (int i = 0; i < N; i++) {
-          double val = b.GetElement(i, 0);//b[i][0];
-          for (int j = 0; j < i; j++) {
-              if (j != i) val -= L.GetElement(i, j) * y.GetElement(j, 0);//L[i][j] * y[j][0];
-          }
-
-          y.SetElement(i, 0, val/L.GetElement(i, i));
-          //y[i][0] = val / L[i][i];
-      }
-
-      //Podstawienie wstecz dla Ux = y
-      for (int i = N - 1; i >= 0; i--) {
-          double val = y.GetElement(i, 0);//y[i][0];
-          for (int j = i; j < N; j++) {
-              if (j != i) val -= U.GetElement(i, j) * x.GetElement(j, 0);//U[i][j] * x[j][0];
-          }
-
-          x.SetElement(i, 0, val/U.GetElement(i, i));
-          //x[i][0] = val / U[i][i];
-      }*/
 
     double error = (A*x - b).Norm();
     std::cout << "Residual error norm for LU decomposition: " << error << std::endl;
 
     return x;
-
-    //auto end = chrono::high_resolution_clock::now();
-    //auto difference = end - start;
-    //*duration = chrono::duration<double, milli>(difference).count();
-
-    //double* r = new double[N];		//residual vector
-    //residual(A, b, x, r);
-    //*n = norm(r);
 }
 
 
@@ -180,8 +119,6 @@ Matrix Jacobi(const Matrix& A, const Matrix& b, int max_iter, double tol, bool s
         r = minus_D.forwardSubstitution(LU * r) + DforwardSubstitutionb;
 
         error[k] = (A*r - b).Norm();
-
-        //std::cout << error[k];
 
         if(error[k] < tol) {
             break;
@@ -303,7 +240,7 @@ void ValuesForPlots(Matrix* A, Matrix* b, int max_iter, double tol, int a1, int 
             b->SetElement(0, j, value);
         }
         GaussSeidelIter[i] = TimeWrapGaussSeidel(*A, *b, max_iter, 1e-6);
-        std::cout << i << ": " << GaussSeidelIter[i] << " ms" << std::endl;
+        std::cout << N[i] << ": " << GaussSeidelIter[i] << " ms" << std::endl;
     }
 
     std::cout << "\n";
@@ -316,7 +253,7 @@ void ValuesForPlots(Matrix* A, Matrix* b, int max_iter, double tol, int a1, int 
             b->SetElement(0, j, value);
         }
         JacobiIter[i] = TimeWrapJacobi(*A, *b, max_iter, 1e-6);
-        std::cout << i << ": " << JacobiIter[i] << " ms" << std::endl;
+        std::cout << N[i] << ": " << JacobiIter[i] << " ms" << std::endl;
     }
 
     //export to csv
